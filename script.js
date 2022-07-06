95edd75b03e726ba001c34e521fd759a74a5decc
Douglas Araújo <goudxs@gmail.com>
2022-07-05 23:07:26 -0300

    Adiciona à página 1 quadra contendo 25 pixels (requisito 4)

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+const pageTitle = document.getElementsByTagName('h1');
+pageTitle[0].setAttribute('id', 'title');
+pageTitle[0].innerHTML = 'Paleta de Cores';
 const  createColorPalette = document.createElement('div');
 createColorPalette.setAttribute('id', 'color-palette');
-const mainTag = document.getElementById('principal');
-mainTag.appendChild(createColorPalette);
+const mainTag = document.getElementsByTagName('main');
+mainTag[0].appendChild(createColorPalette);
 const colorPalette = document.getElementById('color-palette');
 
 function createColor () {
@@ -15,4 +18,24 @@ function createColor () {
 createColor();
 
 colorPalette.firstChild.setAttribute('id', 'firstColor');
+
+const createPixelBoard = document.createElement('div');
+createPixelBoard.setAttribute('id', 'pixel-board');
+mainTag[0].appendChild(createPixelBoard);
+const pixelBoard = document.getElementById('pixel-board');
+
+function createPixel () {
+  let pixel;
+  for (let index = 0; index < 5; index += 1) {
+    const createSection = document.createElement('section');
+    pixelBoard.appendChild(createSection);
+    const linhas = document.getElementsByTagName('section');
+    for (let index2 = 0; index2 < 5; index2 += 1) {
+      pixel = document.createElement('div');
+      pixel.setAttribute('class', 'pixel');
+      linhas[index].appendChild(pixel);
+    }
+  }
+}
+createPixel();
 
