c44c9a596217d579fa7a044834e7425ebc4e0038
Douglas Araújo <goudxs@gmail.com>
2022-07-06 00:11:13 -0300

    Crie uma função para selecionar uma cor na paleta de cores e preencha os pixels no quadro (requisito 7)

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 let firstColor;
-let selected;
+let nowSelected;
+let prevSelected;
 
 const pageTitle = document.getElementsByTagName('h1');
 pageTitle[0].setAttribute('id', 'title');
 pageTitle[0].innerHTML = 'Paleta de Cores';
-const  createColorPalette = document.createElement('div');
+const createColorPalette = document.createElement('div');
 createColorPalette.setAttribute('id', 'color-palette');
 const mainTag = document.getElementsByTagName('main');
 mainTag[0].appendChild(createColorPalette);
 const colorPalette = document.getElementById('color-palette');
 
-function createColor () {
+function createColor() {
   let color;
-  for( let index = 0; index < 4; index += 1) {
+  for (let index = 0; index < 4; index += 1) {
     color = document.createElement('div');
     color.setAttribute('class', 'color');
     colorPalette.appendChild(color);
@@ -27,7 +28,7 @@ createPixelBoard.setAttribute('id', 'pixel-board');
 mainTag[0].appendChild(createPixelBoard);
 const pixelBoard = document.getElementById('pixel-board');
 
-function createPixel () {
+function createPixel() {
   let pixel;
   for (let index = 0; index < 5; index += 1) {
     const createSection = document.createElement('section');
@@ -42,10 +43,21 @@ function createPixel () {
 }
 createPixel();
 
-window.onload = function() {
+function selectColor() {
+  let colors = document.getElementsByClassName('color');
+  prevSelected = document.getElementsByClassName('selected');
+  for (let index = 0; index < colors.length; index += 1) {
+    colors[index].addEventListener('click', function () {
+      if (prevSelected.length !== null) {
+        prevSelected[0].classList.remove('selected');
+      }
+      colors[index].classList.toggle('selected');
+    });
+  }
+}
+selectColor();
+
+window.onload = function () {
   firstColor = document.getElementById('first-color');
-  firstColor.setAttribute('class', 'selected');
+  firstColor.classList.add('selected');
 };
-
-
-
