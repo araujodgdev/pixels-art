de9f20bbc10c7be364d420176a0d9c732825c077
Douglas Araújo <goudxs@gmail.com>
2022-07-06 01:17:20 -0300

    Crie uma função que permita preencher um pixel do quadro com a cor selecionada na paleta de cores (requisito 8)

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 let firstColor;
 let nowSelected;
 let prevSelected;
+let cor;
+let colorValue;
 
 const pageTitle = document.getElementsByTagName('h1');
 pageTitle[0].setAttribute('id', 'title');
 pageTitle[0].innerHTML = 'Paleta de Cores';
+
 const createColorPalette = document.createElement('div');
 createColorPalette.setAttribute('id', 'color-palette');
+
 const mainTag = document.getElementsByTagName('main');
 mainTag[0].appendChild(createColorPalette);
+
 const colorPalette = document.getElementById('color-palette');
+
+const createPixelBoard = document.createElement('div');
+createPixelBoard.setAttribute('id', 'pixel-board');
+mainTag[0].appendChild(createPixelBoard);
+
+const pixelBoard = document.getElementById('pixel-board');
 
 function createColor() {
   let color;
@@ -18,15 +29,9 @@ function createColor() {
     color.setAttribute('class', 'color');
     colorPalette.appendChild(color);
   }
+  colorPalette.firstChild.setAttribute('id', 'first-color');
 }
 createColor();
-
-colorPalette.firstChild.setAttribute('id', 'first-color');
-
-const createPixelBoard = document.createElement('div');
-createPixelBoard.setAttribute('id', 'pixel-board');
-mainTag[0].appendChild(createPixelBoard);
-const pixelBoard = document.getElementById('pixel-board');
 
 function createPixel() {
   let pixel;
@@ -54,10 +59,21 @@ function selectColor() {
       colors[index].classList.toggle('selected');
     });
   }
+  nowSelected = document.getElementsByClassName('selected');
+  firstColor = document.getElementById('first-color');
+  firstColor.classList.add('selected');
 }
 selectColor();
 
-window.onload = function () {
-  firstColor = document.getElementById('first-color');
-  firstColor.classList.add('selected');
-};
+function paintPixel() {
+  let pixels = document.getElementsByClassName('pixel');
+  for (let index = 0; index < pixels.length; index += 1) {
+    pixels[index].addEventListener('click', function () {
+      cor = document.getElementsByClassName('selected');
+      colorValue = window.getComputedStyle(cor[0]).backgroundColor;
+      pixels[index].style.backgroundColor = colorValue;
+    });
+  }
+}
+paintPixel();
+
